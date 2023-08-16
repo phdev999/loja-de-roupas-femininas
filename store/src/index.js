cddf222b6cb6cd0f8f1516f55604fdcbc9a03ccd
Paulo Henryqk <paulohgc2005@gmail.com>
2023-08-16 02:35:58 +0000

    15/08/2023

--- store/src/index.js
+++ store/src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
-import RouterProvider from "react-router-dom"
-import router from './routes';
+// import router from './routes';
 
 import { register } from 'swiper/element/bundle';
 register()
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+  
+  import App from "./App";
+  import ProductDetails from "./components/ProductDetails";
+  
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          path: "/productdetails",
+          element: <ProductDetails />,
+        },
+      ],
+    },
+  ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
